70a7023ea1ea5971b3a6c465d5f361f4b4b745e9
Ustin Vaskin <ustinvaskin@Ustins-MacBook-Pro.local>
2019-09-26 21:44:40 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay= []; 
+
+
+function checkForMatch(){
+
+	if (cardsInPlay[0]=== cardsInPlay[1]){
+		alert ("You found a match!");
+	} else {
+		alert ("Sorry, try again.");
+	}
+} 
+
+function flipCard (cardId){
+	if (cardsInPlay.lenght === 2) {
+		checkForMatch();
+	}
+}
+
+
+cardsInPlay.push(cards[0]);
+console.log("User flipped " + cards[0]); 
+console.log("User flipped " + cards[2]); 
